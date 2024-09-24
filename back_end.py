6ecc4744b533ecfebf5ef4acd3f5dedf057c1554
lauryteey <laurafabiola.s.a@gmail.com>
2024-09-24 18:34:55 +0200

    oppdatering av kalkulatorfunksjoner

--- back_end.py
+++ back_end.py
@@ -1,18 +1,53 @@
-def leggSammen():
-    tall1 = input("Skriv inn det første tallet: ")
-    tall2 = input("Skriv inn det andre tallet: ")
-    sum = int(tall1) + int(tall2)
-    print(f'{tall1} + {tall2} = {sum}')
 
+def leggSammen():
+    try:
+        tall1 = int(input("Skriv inn det første tallet: "))
+        tall2 = int(input("Skriv inn det andre tallet: "))
+        summen = tall1 + tall2
+        print(f'{tall1} + {tall2} = {summen}')
+    except ValueError:
+        print("Vennligst skriv inn gyldige tall.")
+        return
+        
+        
 
 def trekkFra():
-    tall1 = input("Skriv inn det første tallet: ")
-    tall2 = input("Skriv inn det andre tallet: ")
-    diff = int(tall1) - int(tall2)
-    print(f'{tall1} - {tall2} = {diff}')
-
-
-''' Her er noen påbegynte tanker til utvidelse
+    try:
+        tall1 = int(input("Skriv inn det andre tallet: "))
+        tall2 = int(input("Skriv inn det andre tallet: "))
+        diff = int(tall1) - int(tall2)
+        print(f'{tall1} - {tall2} = {diff}')
+    except ValueError:
+        print("Vennligst skriv inn gyldige tall.")
+        return
+        
+        
+    
 def gange():
-    produkt = 3*3
-    TODO: Gjøre ferdig '''
+    try:
+        tall1 = int(input("Skriv inn det andre tallet: "))
+        tall2 = int(input("Skriv inn det andre tallet: "))
+        ganging = tall1 * tall2
+        print(f'{tall1} * {tall2} = {ganging}')
+    except ValueError:
+        print("Bro skriv inn gyldige tall.")
+        return
+        
+        
+def deling():
+    try:
+        tall1 = int(input("skriv inn det første tallet du vil dele: "))
+        tall2 = int(input("Skriv inn det andre tallet du vil dele: "))
+        
+        if tall2 == 0:
+            print("Du kan ikke dele med null xd.")
+            
+        
+        resultat = tall1 / tall2 
+        print(f'{tall1} / {tall2} = {resultat}')
+    except ValueError:
+        print("Vennligst skriv inn gyldige tall. ")
+        return
+    
+
+
