505e4ab04d208944350dd519c3569a8eab9753d2
Julia Mathilde Frenning <156354804+Lolrandomthings@users.noreply.github.com>
2024-09-24 13:22:32 +0200

    fant og fikset print feil

--- back_end.py
+++ back_end.py
@@ -2,14 +2,14 @@ def leggSammen():
     tall1 = input("Skriv inn det første tallet: ")
     tall2 = input("Skriv inn det andre tallet: ")
     sum = int(tall1) + int(tall2)
-    print(f'{tall2} + {tall2} = {sum}')
+    print(f'{tall1} + {tall2} = {sum}')
 
 
 def trekkFra():
     tall1 = input("Skriv inn det første tallet: ")
     tall2 = input("Skriv inn det andre tallet: ")
     diff = int(tall1) - int(tall2)
-    print(f'{tall2} - {tall2} = {diff}')
+    print(f'{tall1} - {tall2} = {diff}')
 
 
 ''' Her er noen påbegynte tanker til utvidelse
